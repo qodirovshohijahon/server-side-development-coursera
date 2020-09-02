89d5a7bbd2fdb9889c18545a74c606d5ef04ee55
qodirovshohijahon <tasaddux@gmail.com>
2020-09-02 12:27:50 +0500

    refactor: With Callback Hell and Promise

--- module-2/mongo-node/index.js
+++ module-2/mongo-node/index.js
@@ -5,39 +5,43 @@ const dboper = require('./operations')
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
 
-MongoClient.connect(url, (err, client) => {
+MongoClient.connect(url).then((client) => {
 
-  assert.equal(err, null);
+  // assert.equal(err, null);
 
   console.log('Connected correctly to server...');
 
   const db = client.db(dbname)
   
-  dboper.insertDocument(db, {name: 'Vadanout', description: 'Test'}, 'dishes', (result) => {
-
+  dboper.insertDocument(db, {name: 'Vadanout', description: 'Test'}, 'dishes')
+  .then((result) => {
     console.log('Insert Document: \n ', result.ops)
 
-    dboper.findDocument(db, 'dishes', (docs) => {
-      console.log('Found Document: \n ', docs);
-
-      dboper.updateDocument(db, {name: 'Vadanout'}, { description: "Updated Test"}, 'dishes', (result) => {
-
-        console.log('Updated Document \n', result.result);
-
-        dboper.findDocument(db, 'dishes', (docs) => {
-          console.log('Found Document: \n ', docs);
-
-          db.dropCollection('dishes', (result) => {
-            console.log('Dopped Collection: ', result);
-
-            client.close();
-          })
-        })
-      })
-    })
+    return dboper.findDocument(db, 'dishes')
   })
-  // const collection = db.collection('dishes')
+  .then((docs) => {
+    console.log('Found Document: \n ', docs);
 
+    return dboper.updateDocument(db, {name: 'Vadanout'}, { description: "Updated Test"}, 'dishes')
+  })
+  .then((result) => {
+    console.log('Updated Document \n', result.result);
+
+    return dboper.findDocument(db, 'dishes')
+  })
+  .then((docs) => {
+    console.log('Found Document: \n ', docs);
+
+    return db.dropCollection('dishes')
+  })
+  .then((result) => {
+    console.log('Dopped Collection: ', result);
+    
+    client.close();
+  })
+  .catch((err) => console.log(err));
+})
+.catch((err) => console.log(err));
   // collection.insertOne({"name": "Uthppizza", "description": "test description"}, (err, result) => {
   //   assert.equal(err, null);
 
@@ -58,4 +62,3 @@ MongoClient.connect(url, (err, client) => {
   //     })
   //   })
   // })
-})
